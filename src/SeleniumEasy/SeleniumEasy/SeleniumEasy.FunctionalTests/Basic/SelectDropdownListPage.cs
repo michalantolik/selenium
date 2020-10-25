ad38eac525fe7e02ff322ab3f45b73700445d2ed
michalantolik <michal.antolik@gmail.com>
2020-10-25 13:53:59 +0100

    Test basic select dropdown

---
+++ src/SeleniumEasy/SeleniumEasy/SeleniumEasy.FunctionalTests/Basic/SelectDropdownListPage.cs
@@ -0,0 +1,97 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using SeleniumEasy.FunctionalTests.BaseClasses;
+using System;
+using System.Linq;
+
+namespace SeleniumEasy.FunctionalTests.Basic
+{
+    public class SelectDropdownListPage : Page
+    {
+        protected override string pageUrl => "https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html";
+        protected override string pageTitle => "Selenium Easy Demo - Automate All Scenarios";
+
+        public SelectDropdownListPage(IWebDriver driver) : base(driver) { }
+
+
+        #region Elements
+
+        private IWebElement dayDropdown => this.driver.FindElement(By.Id("select-demo"));
+        private IWebElement dayStatusParagraph => this.driver.FindElement(By.ClassName("selected-value"));
+
+        private IWebElement stateMultiSelect => this.driver.FindElement(By.Id("multi-select"));
+        private IWebElement firstSelectedButton => this.driver.FindElement(By.Id("printMe"));
+        private IWebElement getAllSelectedButton => this.driver.FindElement(By.Id("printAll"));
+        private IWebElement stateStatusParagraph => this.driver.FindElement(By.ClassName("getall-selected"));
+
+        #endregion Elements
+
+
+        #region Interaction
+
+        public string[] GetDays()
+        {
+            var daySelect = new SelectElement(this.dayDropdown);
+            var days = daySelect.Options
+                .Where(x => x.Text != "Please select")
+                .Select(x => x.Text)
+                .ToArray();
+
+            return days;
+        }
+
+        public void SelectDay(string day)
+        {
+            var daySelect = new SelectElement(this.dayDropdown);
+            daySelect.SelectByText(day);
+        }
+
+        public string GetDayStatusText()
+        {
+            return this.dayStatusParagraph.Text;
+        }
+
+        public void ClickState(string state, bool holdLeftControl = false)
+        {
+            var stateSelect = new SelectElement(this.stateMultiSelect);
+
+            if (holdLeftControl)
+            {
+                var actions = new Actions(this.driver);
+                actions.KeyDown(Keys.LeftControl);
+
+                var stateElement = stateSelect.Options.FirstOrDefault(x => x.Text == state);
+                if (stateElement == null)
+                {
+                    throw new Exception($"No such state: {state}");
+                }
+
+                actions.Click(stateElement);
+                actions.KeyUp(Keys.LeftControl);
+                actions.Perform();
+            }
+            else
+            {
+                stateSelect.SelectByText(state);
+            }
+        }
+
+        public string GetStateStatusText()
+        {
+            return this.stateStatusParagraph.Text;
+        }
+
+        public void ClickFirstSelectedButton()
+        {
+            this.firstSelectedButton.Click();
+        }
+
+        public void ClickGetAllSelectedButton()
+        {
+            this.getAllSelectedButton.Click();
+        }
+
+        #endregion Interaction
+    }
+}
---
+++ src/SeleniumEasy/SeleniumEasy/SeleniumEasy.FunctionalTests/Basic/SelectDropdownListTests.cs
@@ -0,0 +1,69 @@
+﻿using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace SeleniumEasy.FunctionalTests.Basic
+{
+    public class SelectDropdownListTests
+    {
+        [Fact]
+        public void SingleSelectList_SelectDay()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                // Go to page
+                var dropdownListPage = new SelectDropdownListPage(driver);
+                dropdownListPage.NavigateTo();
+
+                // No day selected -> verify status text
+                Assert.Equal($"", dropdownListPage.GetDayStatusText());
+
+                // Select each day one by one -> verify status text
+                var days = dropdownListPage.GetDays();
+                foreach (var day in days)
+                {
+                    dropdownListPage.SelectDay(day);
+                    Assert.Equal($"Day selected :- {day}", dropdownListPage.GetDayStatusText());
+                }
+            }
+        }
+
+        [Fact]
+        public void MultiSelectList_SelectState()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                // Go to page
+                var dropdownListPage = new SelectDropdownListPage(driver);
+                dropdownListPage.NavigateTo();
+
+                // No states selected -> verify status text
+                dropdownListPage.ClickFirstSelectedButton();
+                Assert.Equal("First selected option is : undefined", dropdownListPage.GetStateStatusText());
+
+                dropdownListPage.ClickGetAllSelectedButton();
+                Assert.Equal("Options selected are :", dropdownListPage.GetStateStatusText());
+
+                // Select "Ohio" -> verify status text
+                dropdownListPage.ClickState("Ohio");
+                dropdownListPage.ClickFirstSelectedButton();
+                Assert.Equal("First selected option is : Ohio", dropdownListPage.GetStateStatusText());
+                dropdownListPage.ClickGetAllSelectedButton();
+                Assert.Equal("Options selected are : Ohio", dropdownListPage.GetStateStatusText());
+
+                // Select "Texas" -> verify status text
+                dropdownListPage.ClickState("Texas", holdLeftControl: true);
+                dropdownListPage.ClickFirstSelectedButton();
+                Assert.Equal("First selected option is : Ohio", dropdownListPage.GetStateStatusText());
+                dropdownListPage.ClickGetAllSelectedButton();
+                Assert.Equal("Options selected are : Ohio,Texas", dropdownListPage.GetStateStatusText());
+
+                // Select "Florida" -> verify status text
+                dropdownListPage.ClickState("Florida", holdLeftControl: true);
+                dropdownListPage.ClickFirstSelectedButton();
+                Assert.Equal("First selected option is : Ohio", dropdownListPage.GetStateStatusText());
+                dropdownListPage.ClickGetAllSelectedButton();
+                Assert.Equal("Options selected are : Ohio,Texas,Florida", dropdownListPage.GetStateStatusText());
+            }
+        }
+    }
+}
