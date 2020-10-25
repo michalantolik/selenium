6211d0d681b4a1d2095b4e6dc25e8629842717d0
michalantolik <michal.antolik@gmail.com>
2020-10-25 12:25:47 +0100

    Test basic radio buttons

---
+++ src/SeleniumEasy/SeleniumEasy/SeleniumEasy.FunctionalTests/Basic/RadioButtonsPage.cs
@@ -0,0 +1,130 @@
+﻿using OpenQA.Selenium;
+using SeleniumEasy.FunctionalTests.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SeleniumEasy.FunctionalTests.Basic
+{
+    public class RadioButtonsPage : Page
+    {
+        protected override string pageUrl => "https://www.seleniumeasy.com/test/basic-radiobutton-demo.html";
+        protected override string pageTitle => "Selenium Easy Demo - Radio buttons demo for Automation";
+
+        public RadioButtonsPage(IWebDriver driver) : base(driver) { }
+
+
+        #region Elements
+
+        private IReadOnlyCollection<IWebElement> singleGroupRadioButtons => this.driver.FindElements(By.Name("optradio"));
+        private IWebElement getCheckedValueButton => this.driver.FindElement(By.XPath("//button[text()='Get Checked value']"));
+        private IWebElement singleRadioButtonGroupStatusParagraph => this.driver.FindElement(By.ClassName("radiobutton"));
+
+        private IReadOnlyCollection<IWebElement> genederRadioButtons => this.driver.FindElements(By.Name("gender"));
+        private IReadOnlyCollection<IWebElement> ageGroupRadioButtons => this.driver.FindElements(By.Name("ageGroup"));
+        private IWebElement getValuesButtons => this.driver.FindElement(By.XPath("//button[text()='Get values']"));
+        private IWebElement multipleRadioButtonGroupsStatusParagraph => this.driver.FindElement(By.ClassName("groupradiobutton"));
+
+        #endregion Elements
+
+
+        #region Interaction
+
+        public string[] GetSingleRadioButtonsGroupOptions()
+        {
+            return this.singleGroupRadioButtons.Select(x => x.GetAttribute("value")).ToArray();
+        }
+
+        public bool IsSingleRadioButtonsGroupOptionChecked(string option)
+        {
+            var radiobutton = this.singleGroupRadioButtons.FirstOrDefault(x => x.GetAttribute("value") == option);
+            if (radiobutton == null)
+            {
+                throw new Exception($"No such checkbox option: {option}");
+            }
+            return radiobutton.Selected;
+        }
+
+        public void ClickOptionInSingleRadioButtonGroup(string option)
+        {
+            var radiobutton = this.singleGroupRadioButtons.FirstOrDefault(x => x.GetAttribute("value") == option);
+            if (radiobutton == null)
+            {
+                throw new Exception($"No such checkbox option: {option}");
+            }
+            radiobutton.Click();
+        }
+
+        public string GetSingleRadioButtonGroupStatus()
+        {
+            return this.singleRadioButtonGroupStatusParagraph.Text;
+        }
+
+        public void ClickGetCheckedValueButton()
+        {
+            this.getCheckedValueButton.Click();
+        }
+
+        public string[] GetGenderOptions()
+        {
+            return this.genederRadioButtons.Select(x => x.GetAttribute("value")).ToArray();
+        }
+
+        public string[] GetAgeGroupOptions()
+        {
+            return this.ageGroupRadioButtons.Select(x => x.GetAttribute("value")).ToArray();
+        }
+
+        public bool IsGenderOptionChecked(string option)
+        {
+            var radiobutton = this.genederRadioButtons.FirstOrDefault(x => x.GetAttribute("value") == option);
+            if (radiobutton == null)
+            {
+                throw new Exception($"No such checkbox option: {option}");
+            }
+            return radiobutton.Selected;
+        }
+
+        public bool IsAgeGrouprOptionChecked(string option)
+        {
+            var radiobutton = this.ageGroupRadioButtons.FirstOrDefault(x => x.GetAttribute("value") == option);
+            if (radiobutton == null)
+            {
+                throw new Exception($"No such checkbox option: {option}");
+            }
+            return radiobutton.Selected;
+        }
+
+        public void ClickGenderOption(string option)
+        {
+            var radiobutton = this.genederRadioButtons.FirstOrDefault(x => x.GetAttribute("value") == option);
+            if (radiobutton == null)
+            {
+                throw new Exception($"No such checkbox option: {option}");
+            }
+            radiobutton.Click();
+        }
+
+        public void ClickAgeGroupOption(string option)
+        {
+            var radiobutton = this.ageGroupRadioButtons.FirstOrDefault(x => x.GetAttribute("value") == option);
+            if (radiobutton == null)
+            {
+                throw new Exception($"No such checkbox option: {option}");
+            }
+            radiobutton.Click();
+        }
+
+        public string GetMultipleRadioButtonGroupsStatus()
+        {
+            return this.multipleRadioButtonGroupsStatusParagraph.Text;
+        }
+
+        public void ClickGetValuesButton()
+        {
+            this.getValuesButtons.Click();
+        }
+
+        #endregion Interaction
+    }
+}
---
+++ src/SeleniumEasy/SeleniumEasy/SeleniumEasy.FunctionalTests/Basic/RadioButtonsTests.cs
@@ -0,0 +1,80 @@
+﻿using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace SeleniumEasy.FunctionalTests.Basic
+{
+    public class RadioButtonsTest
+    {
+        [Fact]
+        public void SingleRadioButtonGroup_GetCheckedValue()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                // Go to page
+                var radioButtonsPage = new RadioButtonsPage(driver);
+                radioButtonsPage.NavigateTo();
+
+                // Verify that no option is checked by default
+                var allOptions = radioButtonsPage.GetSingleRadioButtonsGroupOptions();
+                foreach (var singleOption in allOptions)
+                {
+                    Assert.False(radioButtonsPage.IsSingleRadioButtonsGroupOptionChecked(singleOption));
+                }
+
+                // No option is checked -> verify status text
+                radioButtonsPage.ClickGetCheckedValueButton();
+                Assert.Equal("Radio button is Not checked", radioButtonsPage.GetSingleRadioButtonGroupStatus());
+
+                // "Male" option is checked -> verify status text
+                radioButtonsPage.ClickOptionInSingleRadioButtonGroup("Male");
+                radioButtonsPage.ClickGetCheckedValueButton();
+                Assert.Equal("Radio button 'Male' is checked", radioButtonsPage.GetSingleRadioButtonGroupStatus());
+
+                // "Female" option is checked -> verify status text
+                radioButtonsPage.ClickOptionInSingleRadioButtonGroup("Female");
+                radioButtonsPage.ClickGetCheckedValueButton();
+                Assert.Equal("Radio button 'Female' is checked", radioButtonsPage.GetSingleRadioButtonGroupStatus());
+            }
+        }
+
+        [Fact]
+        public void MultipleRadioButtonGroups_GetValues()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                // Go to page
+                var radioButtonsPage = new RadioButtonsPage(driver);
+                radioButtonsPage.NavigateTo();
+
+                // Verify that no options are checked by default
+                var genederOptions = radioButtonsPage.GetGenderOptions();
+                foreach (var gender in genederOptions)
+                {
+                    Assert.False(radioButtonsPage.IsGenderOptionChecked(gender));
+                }
+                var ageGroupOptions = radioButtonsPage.GetAgeGroupOptions();
+                foreach (var ageGroup in ageGroupOptions)
+                {
+                    Assert.False(radioButtonsPage.IsAgeGrouprOptionChecked(ageGroup));
+                }
+
+                // No option is checked -> verify status text
+                radioButtonsPage.ClickGetValuesButton();
+                radioButtonsPage.GetSingleRadioButtonGroupStatus();
+                Assert.Equal("Sex :\r\nAge group:", radioButtonsPage.GetMultipleRadioButtonGroupsStatus());
+
+                // "Female" gender, no age group -> verify status text
+                radioButtonsPage.ClickGenderOption("Female");
+                radioButtonsPage.ClickGetValuesButton();
+                radioButtonsPage.GetSingleRadioButtonGroupStatus();
+                Assert.Equal("Sex : Female\r\nAge group:", radioButtonsPage.GetMultipleRadioButtonGroupsStatus());
+
+                // "Female" gender, "15 to 30" age group -> verify status text
+                radioButtonsPage.ClickAgeGroupOption("15 - 50");
+                radioButtonsPage.ClickGetValuesButton();
+                radioButtonsPage.GetSingleRadioButtonGroupStatus();
+                Assert.Equal("Sex : Female\r\nAge group: 15 - 50", radioButtonsPage.GetMultipleRadioButtonGroupsStatus());
+            }
+        }
+    }
+}
