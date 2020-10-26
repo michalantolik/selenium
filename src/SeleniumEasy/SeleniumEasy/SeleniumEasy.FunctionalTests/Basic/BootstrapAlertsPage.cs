122b3d6969e44f8e191c073fa72209e23d16baa2
michalantolik <michal.antolik@gmail.com>
2020-10-26 16:30:29 +0100

    Test basic bootstrap alerts

---
+++ src/SeleniumEasy/SeleniumEasy/SeleniumEasy.FunctionalTests/Basic/BootstrapAlertsPage.cs
@@ -0,0 +1,69 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumEasy.FunctionalTests.BaseClasses;
+using System;
+
+namespace SeleniumEasy.FunctionalTests.Basic
+{
+    public class BootstrapAlertsPage : Page
+    {
+        protected override string pageUrl => "https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html";
+        protected override string pageTitle => "Selenium Easy - Bootstrap Alerts Demo for Automation";
+
+        public BootstrapAlertsPage(IWebDriver driver) : base(driver) { }
+
+
+        #region Elements
+
+        private IWebElement autocloseSuccessButton => this.driver.FindElement(By.Id("autoclosable-btn-success"));
+        private By autocloseSuccessAlertSelector => By.ClassName("alert-autocloseable-success");
+        private IWebElement autocloseSuccessAlert => this.driver.FindElement(autocloseSuccessAlertSelector);
+
+        private IWebElement normalSuccessButton => this.driver.FindElement(By.Id("normal-btn-success"));
+        private IWebElement normalSuccessAlert => this.driver.FindElement(By.ClassName("alert-normal-success"));
+        private IWebElement normalSuccessAlertCloseButton => this.normalSuccessAlert.FindElement(By.ClassName("close"));
+
+        #endregion Elements
+
+
+        #region Interaction
+
+        public void ClickAutocloseSuccessButton()
+        {
+            this.autocloseSuccessButton.Click();
+        }
+
+        public bool AutocloseSuccessAlertIsDisplayed()
+        {
+            return this.autocloseSuccessAlert.Displayed;
+        }
+
+        public bool AutocloseSuccessAlertIsNotDisplayed(TimeSpan timeout)
+        {
+            WebDriverWait wait = new WebDriverWait(this.driver, timeout);
+            return wait.Until(d =>
+            {
+                var elementList = d.FindElements(this.autocloseSuccessAlertSelector);
+                var elementDisplayed = elementList.Count > 0 && elementList[0].Displayed;
+                return !elementDisplayed;
+            });
+        }
+
+        public void ClickNormalSuccessButton()
+        {
+            this.normalSuccessButton.Click();
+        }
+
+        public bool NormalSuccessAlertIsDisplayed()
+        {
+            return this.normalSuccessAlert.Displayed;
+        }
+
+        public void CloseNormalSuccessAlert()
+        {
+            this.normalSuccessAlertCloseButton.Click();
+        }
+
+        #endregion Interaction
+    }
+}
---
+++ src/SeleniumEasy/SeleniumEasy/SeleniumEasy.FunctionalTests/Basic/BootstrapAlertsTests.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace SeleniumEasy.FunctionalTests.Basic
+{
+    public class BootstrapAlertsTests
+    {
+        [Fact]
+        public void AutocloseSuccessAlert()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                // Go to page
+                var bootstrapAlertsPage = new BootstrapAlertsPage(driver);
+                bootstrapAlertsPage.NavigateTo();
+
+                // Click button to open autoclose success alert
+                bootstrapAlertsPage.ClickAutocloseSuccessButton();
+                Assert.True(bootstrapAlertsPage.AutocloseSuccessAlertIsDisplayed());
+
+                // Wait and verify that alert got auto-closed
+                var timeout = TimeSpan.FromSeconds(10);
+                Assert.True(bootstrapAlertsPage.AutocloseSuccessAlertIsNotDisplayed(timeout));
+            }
+        }
+
+        [Fact]
+        public void NormalSuccessAlert()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                // Go to page
+                var bootstrapAlertsPage = new BootstrapAlertsPage(driver);
+                bootstrapAlertsPage.NavigateTo();
+
+                // Click button to open normal success alert
+                bootstrapAlertsPage.ClickNormalSuccessButton();
+                Assert.True(bootstrapAlertsPage.NormalSuccessAlertIsDisplayed());
+
+                // Close alert with "X" button
+                bootstrapAlertsPage.CloseNormalSuccessAlert();
+
+                // Verify that alert got closed
+                Assert.False(bootstrapAlertsPage.NormalSuccessAlertIsDisplayed());
+            }
+        }
+    }
+}
